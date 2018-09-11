74a80d64ceb152b7bc03cf41a0ecd2b100816106
jyh106 <jyh106@berkeley.edu>
2018-09-10 20:56:02 -0700

    more draggable code

--- ProjectTaskTracker.js
+++ ProjectTaskTracker.js
@@ -1,26 +1,29 @@
 
+/*-------------------------------creating the template-------------------------------------------*/
+
 const addNewProjectButton = document.getElementsByClassName("addButton")[0];
 const addTaskButton = document.getElementsByClassName("addButton")[1];
 
+const deleteProjectButton = document.getElementsByClassName("deleteButton")[0];
+const deleteTaskButton = document.getElementsByClassName('deleteButton')[1];
+
+const projectList = document.getElementsByClassName('sideBar')[0];
 const toDoList = document.getElementsByClassName("toDoList")[0];
 const inProgressList = document.getElementsByClassName('inProgressList')[0];
 const doneList = document.getElementsByClassName('doneList')[0];
-var toDoTaskNumber = 0;
-var projectNumber = 0;
+
+let toDoTaskNumber = 0;
+let projectNumber = 0;
 
 function addToDoTextarea() {
-	const newTask = document.createElement('textarea');
-	newTask.classList.add("createNewTask" + toDoTaskNumber);
-	newTask.classList.add("tasks");
-	newTask.setAttribute("draggable", "true");
+ 	const newTask = document.createElement('textarea');
+	newTask.classList.add("createNewTask" + toDoTaskNumber, "tasks");
 	return newTask;
 }
 
 function addNewProjectTextarea() {
 	const newProjectTextarea = document.createElement('textarea');
-	newProjectTextarea.classList.add("createNewProject" + projectNumber);
-	newProjectTextarea.classList.add("projects");
-	newProjectTextarea.setAttribute("draggable", "true");
+	newProjectTextarea.classList.add("createNewProject" + projectNumber, "projects");
 	return newProjectTextarea
 }
 
@@ -32,7 +35,6 @@ function addToDoTask(){
 
 function addNewProject() {
 	const newProject = addNewProjectTextarea();
-
 	const projectList = document.getElementsByClassName("sideBar")[0];
 	projectList.appendChild(newProject);
 	projectNumber++;
@@ -40,40 +42,56 @@ function addNewProject() {
 
 addTaskButton.addEventListener('click', ()=> addToDoTask());
 addNewProjectButton.addEventListener('click', ()=> addNewProject());
+/*---------------------------------------------------------------------------------------*/
 
-// //make Project elements draggable
-// const projectElement = document.getElementsByClassName('sideBar')[0];
-// const projectDraggable =new Draggable.Draggable(projectElement, {
-// 	draggable: '.projects'
-// 	});
+/*----------------------------develop draggable event------------------------------------*/
+const taskDraggable = new Draggable.Draggable(toDoList, {
+  		draggable: '.tasks'
+	});
 
-// //make tasks draggable
-// const taskElement = document.getElementsByClassName('toDoList')[0];
-// const tasksDraggable = new Draggable.Draggable(taskElement, {
-// 	draggable: '.tasks'
-// 	});
+let currentMousePositionX = 0;
+let currentMousePositionY = 0;
 
-const tasks = document.getElementsByClassName('tasks')[0];
-if(tasks){
-	tasks.on('drag:start', ()=> console.log('dragging'));
-
+function onDragMove(event) {
+	currentMousePositionX = event.sensorEvent.data.clientX;
+	currentMousePositionY = event.sensorEvent.data.clientY;
 }
-// if(tasks){
-// 	for(let task of tasks){
-// 		console.log("here");
-// 		task.on('drag:start', () => console.log('drag:start'));
-// 	}
-// }	
 
-//I NEED HELP HERE!
+function isValidDropPosition(list){
+	const listCoordinates = list.getBoundingClientRect();
+	if((listCoordinates.left < currentMousePositionX < listCoordinates.right) 
+		&& (listCoordinates.bottom < currentMousePositionY < listCoordinates.top)){
+		return true;
+	}
+	return false;
+}
 
-// draggable.on('drag:start', () => console.log('drag:start'));
-// draggable.on('drag:move', () => console.log('drag:move'));
-// draggable.on('drag:stop', () => console.log('drag:stop'));
+function dragStopMove(event){
+	// TODO figure out the list the task came from -- then delete the task 
+		// ---> event.data.sourceContainer.className (parse it, then get second item)
+	// .... then figure out which list it landed at when mouse up, then add it to the list 
+	console.log(event);
+}
 
-const toDoListWidth = toDoList.clientWidth;
-const toDoListHeight = toDoList.clientHeight;
-console.log("width: " + toDoListWidth, "height: " + toDoListHeight);
+// taskDraggable.on('drag:start', () => console.log('drag:start'));
+taskDraggable.on('drag:move', (event) => onDragMove(event));
+taskDraggable.on('drag:stop', (event) => dragStopMove(event)); 
 
+// TODO -- function -- add to the right spot in the list;
 
+/*----------------------------------delete tasks/project button------------------------------*/
+var focusElements = [];
+let currentSelectedElement = document.activeElement;
+focusElements.push(currentSelectedElement);
+
+function deleteTasks(){
+	console.log(focusElements);
+	// const currentSelectedElement = document.activeElement;
+	// currentSelectedElement.parentNode.removeChild(focusElements[focusElements.size() - 2]);
+}
+
+deleteTaskButton.addEventListener('click', ()=>deleteTasks());
+//TODO: add created date to tasks
+
+/*------------------------------------------------------------------------------------------*/
 
